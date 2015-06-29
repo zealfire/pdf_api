9a56107a7d25936c013535d833a68d7017cd4900
zealfire <iamalok185@gmail.com>
2015-06-30 02:45:37 +0530

    Plugin created which interfaces with third party wkhtmltopdf library.

---
+++ src/Plugin/PdfGenerator/WkhtmltopdfGenerator.php
@@ -0,0 +1,130 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\pdf_api\Plugin\WkhtmltopdfGenerator.
+ */
+
+namespace Drupal\pdf_api\Plugin\PdfGenerator;
+
+use Drupal\pdf_api\Plugin\PdfGeneratorBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\pdf_api\Annotation\PdfGenerator;
+use Drupal\Core\Annotation\Translation;
+use mikehaertl\wkhtmlto\Pdf;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * A PDF generator plugin for the WKHTMLTOPDF library.
+ *
+ * @PdfGenerator(
+ *   id = "wkhtmltopdf",
+ *   module = "pdf_api",
+ *   title = @Translation("WKHTMLTOPDF"),
+ *   description = @Translation("PDF generator using the WKHTMLTOPDF binary.")
+ * )
+ */
+class WkhtmltopdfGenerator extends PdfGeneratorBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The global options for WKHTMLTOPDF.
+   *
+   * @var array
+   */
+  protected $options = array();
+
+  /**
+   * Instance of the WKHtmlToPdf class library.
+   *
+   * @var \WkHtmlToPdf
+   */
+  protected $generator;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, array $plugin_definition, Pdf $generator) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->generator = $generator;
+    $this->setOptions(array('binary' => 'C://"Program Files"/wkhtmltopdf/bin/wkhtmltopdf'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('wkhtmltopdf')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setPageOrientation($orientation = PdfGeneratorInterface::PORTRAIT) {
+    $this->setOptions(array('orientation' => $orientation));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setPageSize($page_size) {
+    if ($this->isValidPageSize($page_size)) {
+      $this->setOptions(array('page-size' =>$page_size));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addPage($html) {
+    $this->generator->addPage($html);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function save($location) {
+    $this->preGenerate();
+    $this->generator->send($location);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function send($filename=null) {
+    $this->preGenerate();
+    $this->generator->send($filename);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function stream($html, $filelocation) {
+    $this->preGenerate();
+    $this->generator->saveAs($filelocation);
+  }
+
+  /**
+   * Set global options.
+   *
+   * @param array $options
+   *  The array of options to merge into the currently set options.
+   */
+  protected function setOptions(array $options) {
+    $this->options += $options;
+  }
+
+  /**
+   * Set the global options from the generator plugin into the WKHTMLTOPDF
+   * generator class.
+   */
+  protected function preGenerate() {
+    $this->generator->setOptions($this->options);
+  }
+
+}
---
+++ src/Plugin/PdfGeneratorInterface.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\pdf_api\Plugin\PdfGeneratorInterface
+ */
+
+namespace Drupal\pdf_api\Plugin;
+
+/**
+ * Defines an interface for PDF generator plugins.
+ */
+interface PdfGeneratorInterface {
+
+  /**
+   * Landscape paper orientation.
+   */
+  const LANDSCAPE = 'landscape';
+
+  /**
+   * Portrait paper orientation.
+   */
+  const PORTRAIT = 'portrait';
+
+  /**
+   * Returns the administrative label for this generator plugin.
+   *
+   * @return string
+   */
+  public function getLabel();
+
+  /**
+   * Returns the administrative description for this generator plugin.
+   *
+   * @return string
+   */
+  public function getDescription();
+
+  /**
+   * Set the paper orientation of the generated PDF pages.
+   *
+   * @param PdfGeneratorInterface::PORTRAIT|PdfGeneratorInterface::LANDSCAPE $orientation
+   *  The orientation of the PDF pages.
+   */
+  public function setPageOrientation($orientation = PdfGeneratorInterface::PORTRAIT);
+
+  /**
+   * Set the page size of the generated PDF pages.
+   *
+   * @param string $page_size
+   *  The page size (e.g. A4, B2, Letter).
+   */
+  public function setPageSize($page_size);
+
+  /**
+   * Add a page to the generated PDF.
+   *
+   * @param string $html
+   *  The HTML of the page to be added.
+   */
+  public function addPage($html);
+
+  /**
+   * Generate and save the PDF at a specific location.
+   *
+   * @param string $location
+   *  The location path to save the generated PDF to.
+   */
+  public function save($location);
+
+  /**
+   * Send the PDF to the browser has a file download.
+   *
+   * @param string $filename
+   *  The filename to send the file to the browser with.
+   */
+  public function send($filename);
+
+  /**
+   * Stream the PDF to the browser.
+   */
+  public function stream($html, $filelocation);
+}
